2284d9bbaad443882ecf312c7a4be4d339bebf1f
oShyyy1 <83133779+oShyyy1@users.noreply.github.com>
2021-04-24 23:24:38 +0200

    Bot DISC en dev.
    Bot discrod

---
+++ index.js
@@ -0,0 +1,16 @@
+const Discord = require('discord.js');
+const  bot = new Discord.Client();
+const token = require("./token.json")
+
+
+bot.on("ready", asyinc ( =>{
+    console.log("Le bot est allumer");
+    bot.user.setStatus("online");
+    bot.user.setActivity(A rien faire ;-;), {type: 'STREAMING'} );
+    )}
+    
+
+
+
+
+client.login(process.env.TOKEN);
